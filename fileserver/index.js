9a47ca17cf985897c2f7692e6fdfdb82966aed91
Richard Boulton <richard@tartarus.org>
2018-01-24 09:22:53 +0000

    Sketch some startup code

--- fileserver/index.js
+++ fileserver/index.js
@@ -1,2 +1,39 @@
-export default () => {
+const { Store } = require('./store')
+const { Api } = require('./api')
+
+function run (store, api) {
+  api.serve()
 }
+
+async function gracefulShutdown (store, api) {
+  console.log('Shutting down')
+  try {
+    await api.end()
+    await store.end()
+  } catch (err) {
+    console.log('Error when shutting down: ' + err)
+  }
+}
+
+(async () => {
+  const store = new Store()
+  const api = new Api(store)
+
+  try {
+    await store.init()
+    await api.init()
+  } catch (err) {
+    console.log('Failed to start up: ' + err)
+    process.exitCode = 1
+    try {
+      await gracefulShutdown(store, api)
+    } catch (err) {
+      console.log('Unable to shut down cleanly: ' + err)
+      process.exit(1)
+    }
+  }
+
+  run(store, api)
+})().catch(err => {
+  console.log(err)
+})
